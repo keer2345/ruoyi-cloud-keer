a70d90562ee83effdcbf2222131168896c2f24cb
keer2345 <keer2345@gmail.com>
2023-08-15 20:58:15 +0800

    ruoyi-monitor

---
+++ ruoyi-visual/ruoyi-monitor/src/main/java/com/keyi/module/monitor/RuoYiMonitorApplication.java
@@ -0,0 +1,16 @@
+package com.keyi.module.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author keer
+ * @date 2023-08-15
+ */
+@SpringBootApplication
+public class RuoYiMonitorApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RuoYiMonitorApplication.class, args);
+    System.out.println("(♥◠‿◠)ﾉﾞ  监控中心启动成功   ლ(´ڡ`ლ)ﾞ  ");
+  }
+}
